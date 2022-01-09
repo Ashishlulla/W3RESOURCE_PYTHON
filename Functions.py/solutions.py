e14fe19fc9e4c530248a8648bf9082c458c7bc12
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-09 17:12:38 +0530

    Update solutions.py

--- Functions.py/solutions.py
+++ Functions.py/solutions.py
@@ -19,3 +19,33 @@ def my_sum(lst):
     return add
 
 print(my_sum([1,2,3,4,5]))
+
+# 3. Write a Python function to multiply all the numbers in a list. Go to the editor
+# Sample List : (8, 2, 3, -1, 7)
+# Expected Output : -336
+def my_prod(lst):
+    mul = 1
+    for i in lst:
+        mul *= i
+    return mul
+print(my_prod([1, 2, 3, 4, 5]))
+
+
+# 4. Write a Python program to reverse a string. Go to the editor
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+
+def my_reverse(string):
+    return string[::-1]
+
+
+# 5. Write a Python function to calculate the factorial of a number.
+# The function accepts the number as an argument.
+
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+print(factorial(5))
